d79b8be9c57cf889f97f065d324b039fe52cd10d
jitpz <georgies1me@gmail.com>
2022-01-22 16:58:02 -0300

    Make board module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { BoardModule } from './modules/board/board.module';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    BoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
