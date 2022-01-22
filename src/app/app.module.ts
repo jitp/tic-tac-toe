f6e1d96f11d78cd5c50b4535513a32ca67060a22
jitpz <georgies1me@gmail.com>
2022-01-22 17:25:28 -0300

    Create app header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CoreModule } from '@app/core.module';
+import { BoardModule } from '@modules/board/board.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { BoardModule } from './modules/board/board.module';
+import { HeaderComponent } from './layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
