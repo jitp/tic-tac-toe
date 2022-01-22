f4b060595e348960a60c68074621c171e501e301
jitpz <georgies1me@gmail.com>
2022-01-22 19:30:25 -0300

    Update board module with page and routing

--- src/app/modules/board/board.module.ts
+++ src/app/modules/board/board.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
+import { BoardPageComponent } from './pages/board-page/board-page.component';
+import { BoardRoutingModule } from './board-routing.module';
 
 @NgModule({
-  declarations: [],
-  imports: [SharedModule],
+  declarations: [BoardPageComponent],
+  imports: [SharedModule, BoardRoutingModule],
 })
 export class BoardModule {}
