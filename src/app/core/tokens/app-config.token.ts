fe2e2632584ed717e58c04f6832926e0cdfe8d5f
jitpz <georgies1me@gmail.com>
2022-01-22 19:53:37 -0300

    Define token for app config

---
+++ src/app/core/tokens/app-config.token.ts
@@ -0,0 +1,9 @@
+import { InjectionToken } from "@angular/core";
+import { AppConfig } from "@app/interfaces";
+
+export const APP_CONFIG = new InjectionToken<AppConfig>("app.config", {
+    providedIn: "root",
+    factory: () => ({
+        boardSize: 9
+    })
+})
