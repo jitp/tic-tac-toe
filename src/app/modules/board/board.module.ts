8ccccc04b22f1150934b1357592f5a19f8070b00
jitpz <georgies1me@gmail.com>
2022-01-22 17:09:19 -0300

    Make shared module
    - It also incorporates path aliases to main places in the app

--- src/app/modules/board/board.module.ts
+++ src/app/modules/board/board.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [SharedModule],
 })
-export class BoardModule { }
+export class BoardModule {}
